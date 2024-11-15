1a2d474f86c9bd7b494122bac2113dbea3d28053
chani74 <chani74@gmail.com>
2024-11-15 15:15:30 +0900

    commit

---
+++ src/test/TestGit1.java
@@ -0,0 +1,6 @@
+package test;
+
+public class TestGit1 {
+	String test = "연습1번";
+	String test1 = "연습1-1번"; 
+}
---
+++ src/test/TestGit2.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestGit2 {
+	String test = "연습2번";
+	String test2 = "연습2번-1번";  
+}
+
